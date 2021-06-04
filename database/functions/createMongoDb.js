2a2c964d43ddd6012ac5122cb88898fa10d981fb
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2021-06-04 18:05:04 +0530

    Fixed issue with ID

--- database/functions/createMongoDb.js
+++ database/functions/createMongoDb.js
@@ -3,29 +3,34 @@ const { getDb } = require("../db");
 
 const createMongoDb = async (id, port) => {
     return new Promise(async (resolve, reject) => {
-        exec(`docker container run -p ${port}:27017 --name ${id} -d mongo`, async(error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                reject(error.message)
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                reject(error.message)
-                return;
-            }
-            const db = await getDb();
-
-            db.collection('databases').updateOne(
-                { id: _id },
-                { $set: { address: `mongodb://34.197.98.169:${port}`, } },
-                function (err, _) {
-                    if (err) return reject(err);
+        try {
+            exec(`docker container run -p ${port}:27017 --name ${id} -d mongo`, async (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    reject(error.message)
+                    return;
                 }
-            )
-            console.log(`stdout: ${stdout}`);
-            return resolve(stdout);
-        });
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    reject(error.message)
+                    return;
+                }
+                const db = await getDb();
+
+                db.collection('databases').updateOne(
+                    { _id: id },
+                    { $set: { address: `mongodb://34.197.98.169:${port}`, } },
+                    function (err, _) {
+                        if (err) return reject(err);
+                    }
+                )
+                console.log(`stdout: ${stdout}`);
+                return resolve(stdout);
+            });
+        } catch (err) {
+           console.log(err) ;
+           return reject(err);
+        }
     })
 }
 
