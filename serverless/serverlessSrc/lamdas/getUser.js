de3bd795a64a525bb2b60f979b88648b0c7f3646
Raghav Roy <raghavroy145@gmail.com>
2021-06-05 10:23:03 +0530

    API fixes

--- serverless/serverlessSrc/lamdas/getUser.js
+++ serverless/serverlessSrc/lamdas/getUser.js
@@ -1,8 +1,12 @@
 const Responses = require('./API_Responses')
-
+const data = {
+    1234: { name: 'Sourav Tekken', age: 21, job: 'astronaut' },
+    5678: { name: 'Ashwin Alaparthi', age: 21, job: 'instagram influencer' },
+    9101: { name: 'Ritik Hariani', age: 21, job: 'CEO OpenCookBook' },
+};
 exports.handler = async event =>  {
     console.log('event', event)
-
+    return Responses._200(data[ID])
     if(!event.pathParameters || !event.pathParameters.ID) {
         //failed without an ID
         return Responses._400({message: 'missing the ID from the path'})
@@ -17,10 +21,4 @@ exports.handler = async event =>  {
 
     //failed as ID not in the data
     return Responses._400({message: 'no ID in data'})
-};
-
-const data = {
-    1234: { name: 'Sourav Tekken', age: 21, job: 'astronaut' },
-    5678: { name: 'Ashwin Alaparthi', age: 21, job: 'instagram influencer' },
-    9101: { name: 'Ritik Hariani', age: 21, job: 'CEO OpenCookBook' },
 };
